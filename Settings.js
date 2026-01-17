5485613a673cd66a6cfe0eae437948b8280aa977
LORD PREMO <cypherx599@gmail.com>
2026-01-17 18:01:36 +0300

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//contact details
+global.ownernomer = "255688422125"
+global.ownername = "LORD PREMO 👑"
+global.ytname = "YT: LordPremo"
+global.socialm = "GitHub: lordpremo"
+global.location = "Tanzania, Zanzibar"
+
+global.ownernumber = '255688422125'  //creator number
+global.ownername = 'LORD PREMO 👑'   //owner name
+global.botname = 'LORD PREMO XX BUG BOT' //name of the bot
+
+//sticker details
+global.packname = 'Sticker By'
+global.author = 'LORD PREMO 👑\n\nContact: +255688422125'
+
+//console view/theme
+global.themeemoji = '⚡'
+global.wm = "LORD PREMO."
+
+//theme link
+global.link = 'https://whatsapp.com/channel/0029VbBkQRi30LKNq4vrw63F'
+
+//custom prefix
+global.prefa = ['','!','.','#','&']
+
+//false=disable and true=enable
+global.autoRecording = false //auto recording
+global.autoTyping = false //auto typing
+global.autorecordtype = false //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = true //auto update bio
+global.anti92 = false //auto block +92 
+global.autoswview = false //auto view status/story
+
+//menu type 
+//v1 is image menu, 
+//v2 is link + image menu,
+//v3 is video menu,
+//v4 is call end menu
+global.typemenu = 'v1'
+
+//text bug
+global.xbugtex = {
+  xtxt: '🚨 LORD PREMO XX BUG 🚨',
+}
+
+//image bug placeholder
+global.bimg = 'https://files.catbox.moe/4wmhq6.jpg'
