4c8b8d8ed66e793b13b539d279f1ad228ee3e08e
LORD PREMO <cypherx599@gmail.com>
2026-01-17 17:04:08 +0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,30 @@
+//Base by Lord Premo
+//GitHub: @LordPremo
+//WhatsApp: +255688 422 125
+//Email: lordpremo8@gmail.com
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
